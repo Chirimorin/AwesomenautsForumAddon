2e83a33376074a4b2f7d3a2fdf6f46c8213ab8af
Thomas Voogt <chirimorin@gmail.com>
2013-05-27 11:27:47 +0200

    Proper auto update script

--- AwesomenautsForumAutoUpdate.user.js
+++ AwesomenautsForumAutoUpdate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Auto updating Awesomenauts forum addon
 // @description   Auto updating version of the Awesomenauts forum addon. 
-// @version       2.0
+// @version       1.0
 // @author        Chirimorin
 // @run-at        document-end
 // @match         http://www.awesomenauts.com/forum*
@@ -9,10 +9,8 @@
 // ==/UserScript==
 
 if(window.top == window) {
-alert('test1');
   var script = document.createElement("script");
   script.type = "text/javascript";
   script.src = "https://github.com/Chirimorin/AwesomenautsForumAddon/raw/master/AwesomenautsForum.js"
   document.body.appendChild(script);
-  alert('test2');
 } 
