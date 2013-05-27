0c56082bc66f0d35eba0c9ccec5b3eef68992cc5
Thomas Voogt <chirimorin@gmail.com>
2013-05-27 12:30:03 +0200

    Test version links

--- AwesomenautsForumTestVersion.js
+++ AwesomenautsForumTestVersion.js
@@ -1,21 +1,46 @@
-alert(event.shiftKey);
+//Changing the links so test mode is kept even when changing pages. 
+var links = document.evaluate
+                            (
+                                '//a', 
+                                document, 
+                                null, 
+                                XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, 
+                                null
+                            ); 
+
+for (var i=0; i < links.snapshotLength; i++) 
+{ 
+    var thisLink = links.snapshotItem(i); 
+    
+    if (thisLink.href.search.indexOf('forum') != -1) //make sure the link is to a forum page
+    {
+        if ( thisLink.href.search.indexOf('?') == -1 ) 
+        {
+            thisLink.href += '?ForumScriptTest=1';
+        }
+        else
+        {
+            thisLink.href += '&ForumScriptTest=1';
+        }
+    }
+} 
 
 //Thanks to Nodja for the code to keep onclick behavior. 
 // gets all td elements with class="row1 clickable"
 var allClickables = document.evaluate
                                     (
-                                      '//td[@class="row1 clickable"]',
-                                      document, 
-                                      null,
-                                      XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-                                      null
+                                        '//td[@class="row1 clickable"]',
+                                        document, 
+                                        null,
+                                        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+                                        null
                                     );
 
 //replaces the onclick to check for left button and ctrl key
 for (var i=0; i<allClickables.snapshotLength; i++) 
 {
-  var elem = allClickables.snapshotItem(i);
-  elem.setAttribute("onclick","if (event.button == 0 && event.ctrlKey == false) " + elem.getAttribute("onclick"));
+    var elem = allClickables.snapshotItem(i);
+    elem.setAttribute("onclick","if (event.button == 0 && event.ctrlKey == false) " + elem.getAttribute("onclick"));
 }
 //End of Nodjas script
 
@@ -27,16 +52,17 @@ for (i=0; i<postBodys.length ; i++)
     for (j=0; j<imgs.length; j++) 
     {
         imgs[j].style.maxWidth = '700px';
-		imgs[j].addEventListener('click', function(event) 
+        imgs[j].addEventListener('click', function(event) 
             {
-				if (event.currentTarget.style.maxWidth == 'none') 
+                if (event.currentTarget.style.maxWidth == 'none') 
                 {
-					event.currentTarget.style.maxWidth = '700px';
-				} 
+                    event.currentTarget.style.maxWidth = '700px';
+                } 
                 else 
                 {
-					event.currentTarget.style.maxWidth = 'none';
-				}
-			}, false);
+                    event.currentTarget.style.maxWidth = 'none';
+                }
+            }, false);
     }
 }
+
