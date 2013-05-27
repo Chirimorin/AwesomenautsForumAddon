7aabe4e63ba2620bb8803404ce3d96a41b991c6e
Thomas Voogt <chirimorin@gmail.com>
2013-05-27 11:26:07 +0200

    Auto update test

---
+++ AwesomenautsForum.js
@@ -0,0 +1,40 @@
+//Thanks to Nodja for the code to keep onclick behavior. 
+// gets all td elements with class="row1 clickable"
+var allClickables = document.evaluate
+                                    (
+                                      '//td[@class="row1 clickable"]',
+                                      document, 
+                                      null,
+                                      XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+                                      null
+                                    );
+
+//replaces the onclick to check for left button and ctrl key
+for (var i=0; i<allClickables.snapshotLength; i++) 
+{
+  var elem = allClickables.snapshotItem(i);
+  elem.setAttribute("onclick","if (event.button == 0 && event.ctrlKey == false) " + elem.getAttribute("onclick"));
+}
+//End of Nodjas script
+
+
+var postBodys = document.getElementsByClassName('postbody');
+for (i=0; i<postBodys.length ; i++) 
+{
+    var imgs = postBodys[i].getElementsByTagName('img');
+    for (j=0; j<imgs.length; j++) 
+    {
+        imgs[j].style.maxWidth = '700px';
+		imgs[j].addEventListener('click', function(event) 
+            {
+				if (event.currentTarget.style.maxWidth == 'none') 
+                {
+					event.currentTarget.style.maxWidth = '700px';
+				} 
+                else 
+                {
+					event.currentTarget.style.maxWidth = 'none';
+				}
+			}, false);
+    }
+}
--- AwesomenautsForumAutoUpdate.user.js
+++ AwesomenautsForumAutoUpdate.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name          Awesomenauts forum addon
+// @name          Auto updating Awesomenauts forum addon
 // @description   Auto updating version of the Awesomenauts forum addon. 
 // @version       2.0
 // @author        Chirimorin
@@ -9,8 +9,10 @@
 // ==/UserScript==
 
 if(window.top == window) {
+alert('test1');
   var script = document.createElement("script");
   script.type = "text/javascript";
   script.src = "https://github.com/Chirimorin/AwesomenautsForumAddon/raw/master/AwesomenautsForum.js"
   document.body.appendChild(script);
+  alert('test2');
 } 
