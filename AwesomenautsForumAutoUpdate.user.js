d377733930a7ff20de0f6804513d2182744607bc
Thomas Voogt <chirimorin@gmail.com>
2013-05-27 11:15:09 +0200

    Auto update version fix

---
+++ AwesomenautsForumAutoUpdate.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          Awesomenauts forum addon
+// @description   Auto updating version of the Awesomenauts forum addon. 
+// @version       2.0
+// @author        Chirimorin
+// @run-at        document-end
+// @match         http://www.awesomenauts.com/forum*
+// @match         https://www.awesomenauts.com/forum*
+// ==/UserScript==
+
+if(window.top == window) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://github.com/Chirimorin/AwesomenautsForumAddon/raw/master/AwesomenautsForum.js"
+  document.body.appendChild(script);
+} 
