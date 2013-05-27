612b49f005971891d0c74f3314f0363181b31b8d
Thomas Voogt <chirimorin@gmail.com>
2013-05-27 11:39:47 +0200

    Auto updating old script
    Needed for easy upgrading.

--- AwesomenautsForum.user.js
+++ AwesomenautsForum.user.js
@@ -1,50 +1,16 @@
 // ==UserScript==
 // @name          Awesomenauts forum addon
-// @description   Allows for the usage of middle click and ctrl+click without problems. Also fixes images that are too big. 
-// @version       1.7
+// @description   See http://www.awesomenauts.com/forum/viewtopic.php?f=6&t=14730 for more info.
+// @version       2.0
 // @author        Chirimorin
 // @run-at        document-end
 // @match         http://www.awesomenauts.com/forum*
 // @match         https://www.awesomenauts.com/forum*
 // ==/UserScript==
 
-//Thanks to Nodja for the code to keep onclick behavior. 
-// gets all td elements with class="row1 clickable"
-var allClickables = document.evaluate
-                                    (
-                                      '//td[@class="row1 clickable"]',
-                                      document, 
-                                      null,
-                                      XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-                                      null
-                                    );
-
-//replaces the onclick to check for left button and ctrl key
-for (var i=0; i<allClickables.snapshotLength; i++) 
-{
-  var elem = allClickables.snapshotItem(i);
-  elem.setAttribute("onclick","if (event.button == 0 && event.ctrlKey == false) " + elem.getAttribute("onclick"));
-}
-//End of Nodjas script
-
-
-var postBodys = document.getElementsByClassName('postbody');
-for (i=0; i<postBodys.length ; i++) 
-{
-    var imgs = postBodys[i].getElementsByTagName('img');
-    for (j=0; j<imgs.length; j++) 
-    {
-        imgs[j].style.maxWidth = '700px';
-		imgs[j].addEventListener('click', function(event) 
-            {
-				if (event.currentTarget.style.maxWidth == 'none') 
-                {
-					event.currentTarget.style.maxWidth = '700px';
-				} 
-                else 
-                {
-					event.currentTarget.style.maxWidth = 'none';
-				}
-			}, false);
-    }
-}
+if(window.top == window) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://github.com/Chirimorin/AwesomenautsForumAddon/raw/master/AwesomenautsForum.js"
+  document.body.appendChild(script);
+} 
