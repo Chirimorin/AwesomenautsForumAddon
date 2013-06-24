cad312449884ce538e786b323aa919dbcb51bc7d
Thomas Voogt <chirimorin@gmail.com>
2013-06-24 14:35:07 +0200

    Test version custom smileys

--- AwesomenautsForumTestVersion.js
+++ AwesomenautsForumTestVersion.js
@@ -86,3 +86,105 @@ for (i=0; i<postBodys.length ; i++)
     }
 }
 
+//extra smilies code by Nodja
+var smilieslist = new Array();
+smilieslist.push({name: "Duck (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/ducks10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/ducks10.gif[/img]"});
+smilieslist.push({name: "Prestige 10 (by RiceMaster)", url: "http://i.imgur.com/BBctJc8.gif", code: "[img]http://i.imgur.com/BBctJc8.gif[/img]"});
+smilieslist.push({name: "League 1 (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/lg1_g11.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/lg1_g11.gif[/img]"});
+smilieslist.push({name: "League 2 (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/lg210.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/lg210.gif[/img]"});
+smilieslist.push({name: "League 3 (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/lg310.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/lg310.gif[/img]"});
+smilieslist.push({name: "League 4 (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/lg410.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/lg410.gif[/img]"});
+smilieslist.push({name: "Voltar (by conorbebe)", url: "http://i.imgur.com/KDfPpJQ.gif", code: "[img]http://i.imgur.com/KDfPpJQ.gif[/img]"});
+smilieslist.push({name: "Creepy Leon (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/creep_10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/creep_10.gif[/img]"});
+smilieslist.push({name: "Derpl (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/derpl_10.png", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/derpl_10.png[/img]"});
+smilieslist.push({name: "Rae YJM(by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/rae_ma10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/rae_ma10.gif[/img]"});
+smilieslist.push({name: "Yummy Skolldir (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/yum_sk11.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/yum_sk11.gif[/img]"});
+smilieslist.push({name: "Blow spike (by conorbebe)", url: "http://i.imgur.com/jm1zqZd.gif", code: "[img]http://i.imgur.com/jm1zqZd.gif[/img]"});
+smilieslist.push({name: "Vinnie (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/vini_d10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/vini_d10.gif[/img]"});
+smilieslist.push({name: "Vinnie Face (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/vini_h10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/vini_h10.gif[/img]"});
+smilieslist.push({name: "Vinnie Clap (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/vini_a11.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/vini_a11.gif[/img]"});
+smilieslist.push({name: "Gnaw (by conorbebe)", url: "http://i.imgur.com/gZ89B3d.gif", code: "[img]http://i.imgur.com/gZ89B3d.gif[/img]"});
+smilieslist.push({name: "Gnaw spit (by conorbebe)", url: "http://i.imgur.com/M50popG.gif", code: "[img]http://i.imgur.com/M50popG.gif[/img]"});
+smilieslist.push({name: "Toast (by RiceMaster)", url: "http://i83.servimg.com/u/f83/17/49/01/19/toooas10.gif", code: "[img]http://i83.servimg.com/u/f83/17/49/01/19/toooas10.gif[/img]"});
+
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+if(window.location.href.indexOf("posting.php") > -1) //figure out if we're posting
+{
+   var form = document.getElementsByName('postform')[0];
+   if (form.attachEvent) {
+      form.attachEvent("submit", submitHandler);
+   } else {
+      form.addEventListener("submit", submitHandler);
+   }
+   
+   var message = document.getElementsByName("message")[0].value;
+
+   
+   for(i=0;i<smilieslist.length;i++)
+   {
+      var re = new RegExp(escapeRegExp("[img]" + smilieslist[i].url  + "[/img]"),"gi");
+      message = message.replace(re, smilieslist[i].code);
+   }
+   document.getElementsByName("message")[0].value = message;
+   
+   //get all row1 tds
+   var allRow1 = document.evaluate
+                                    (
+                                      '//td[@class="row1"]',
+                                      document, 
+                                      null,
+                                      XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+                                      null
+                                    );
+   for (var i=0; i<allRow1.snapshotLength; i++) 
+   {
+      var elem = allRow1.snapshotItem(i);
+      if (elem.innerHTML.indexOf("Message body:") > -1) break;
+   }
+   elem.innerHTML += "<table width=\"100%\" cellspacing=\"5\" cellpadding=\"0\" border=\"0\" align=\"center\"> \
+      <tbody> \
+         <tr> \
+            <td class=\"gensmall\" align=\"center\"><b>Custom Smilies</b></td> \
+         </tr> \
+         <tr> \
+            <td align=\"center\" id=\"customsmilies\"> \
+            </td> \
+         </tr> \
+      </tbody> \
+   </table>";
+   drawSmilies();
+}
+
+function submitHandler(e)
+{
+   var message = document.getElementsByName("message")[0].value;
+   
+   for(i=0;i<smilieslist.length;i++)
+   {
+      var re = new RegExp(smilieslist[i].code,"gi");
+      message = message.replace(re, "[img]" + smilieslist[i].url + "[/img]");
+   }
+   document.getElementsByName("message")[0].value = message;
+    return false;
+}
+
+function drawSmilies()
+{
+   var smilieshtml = '';
+   for(i=0;i<smilieslist.length;i++)
+   {
+      smilieshtml += "<a href=\"#\" onclick=\"insert_text('";
+      smilieshtml += smilieslist[i].code + "', true); return false;\" style=\"line-height: 20px;\"><img src=\"";
+      smilieshtml += smilieslist[i].url + "\" alt=\"";
+      smilieshtml += smilieslist[i].code + "\" title=\"";
+      smilieshtml += smilieslist[i].name + "\" hspace=\"2\" vspace=\"2\"></a>\n";
+   }
+   document.getElementById("customsmilies").innerHTML = smilieshtml;
+}
+
+
+
