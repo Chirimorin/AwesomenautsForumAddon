4c032301b8f2421e39dff2c185d66e565689aa01
Thomas Voogt <chirimorin@gmail.com>
2014-04-28 14:32:06 +0200

    Shoutbox jQuery

--- Shoutbox.js
+++ Shoutbox.js
@@ -1,2 +1,20 @@
 console.log("Shoutbox script loaded");
 
+var checker = 0;
+function jqueryLoaded() {
+    clearInterval(checker);
+    console.log("Shoutbox jQuery found; running script...");
+    
+}
+
+function checkJquery() {
+    if (window.jQuery) {
+            jqueryLoaded();
+    } 
+        if(checker == 0) {
+            console.log("Waiting for shoutbox jQuery to load...");
+            checker = window.setInterval(checkJquery, 100);
+        }
+}	
+
+checkJquery();
