9387e233034ef4668ee124d550b4b90f005abf13
Thomas Voogt <chirimorin@gmail.com>
2014-05-02 13:19:13 +0200

    Revert "MOTD"
    This reverts commit f2c9b5c2e8a429659b575743199af1c1cc3f4f84.

--- Shoutbox.js
+++ Shoutbox.js
@@ -1,7 +1,7 @@
 console.log("Shoutbox script loaded");
 
 var currentVersion = 1.04;
-var MOTD = "Sentry Live stream tonight! <a href=\"http://www.timeanddate.com/counters/fullscreen.html?mode=a&iso=20140502T20&year=2014&month=5&day=2&hour=20&min=0&sec=0&p0=1310&msg=Ronimo%20Livestream\">Countdown here!</a>";
+var MOTD = "Live stream tonight! <a href=\"http://www.timeanddate.com/counters/fullscreen.html?mode=a&iso=20140502T20&year=2014&month=5&day=2&hour=20&min=0&sec=0&p0=1310&msg=Ronimo%20Livestream\">Countdown here!</a>";
 
 var Ronimo = new Array();
 Ronimo.push("Jasper");
