1ab772fb3b8b9e72fbe321fb3638dd381e3c806a
Thomas Voogt <chirimorin@gmail.com>
2014-05-14 18:23:24 +0200

    Avatar sort test

--- AwesomenautsForumTest.js
+++ AwesomenautsForumTest.js
@@ -15,3 +15,15 @@ function optionsLoaded() {
 //Test functionality starts here
 
 
+if ($('select[name=category]').val() == "Awesomenauts")
+{
+    var table = $('img[title^=Avatar]').first().parent().parent().parent().parent();
+    var row = $(table).parent();
+    console.log(row);
+    $('img[title^=Avatar]').sort(function(a,b) {
+        return (($(a).attr('title') > $(b).attr('title')) ? 1 : -1);
+    }).each(function() {
+        $(row).append("<div style=\"float: left; border: 1px solid #aaaaaa; height: 130px; width: 102px;\">"+$('<div>').append($(this).clone()).html()+"<br /><input type=\"radio\" name=\"avatar_select\" value=\""+$(this).attr('src').split('/').pop()+"\"></div>");
+    });
+    $(table).remove();
+}
